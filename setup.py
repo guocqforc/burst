dd98de06dca78c501ace7447d00985d03a981130
dantezhu <dantezhu@qq.com>
2016-02-01 15:17:22 +0800

    burst

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="melon",
+    name="burst",
     version='1.0.79',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=['twisted', 'events'],
-    url="https://github.com/dantezhu/melon",
+    url="https://github.com/dantezhu/burst",
     license="MIT",
     author="dantezhu",
     author_email="zny2008@gmail.com",
