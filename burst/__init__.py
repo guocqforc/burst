6ea9396ec87793586340ff6f90136b32f204dc91
dantezhu <dantezhu@qq.com>
2016-02-01 19:22:56 +0800

    blueprint

--- burst/__init__.py
+++ burst/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = '1.0.79'
 
-from .burst import burst
-from .worker import Blueprint
+from .burst import Burst
+from .blueprint import Blueprint
 from .log import logger
 from .utils import safe_call, safe_func
