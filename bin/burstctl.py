0684806bcbe5fc5975119691884e2c96e7b8b79b
dantezhu <dantezhu@qq.com>
2016-02-04 13:01:20 +0800

    改成用类的方式实现

--- bin/burstctl.py
+++ bin/burstctl.py
@@ -9,15 +9,92 @@ from netkit.contrib.tcp_client import TcpClient
 from burst import constants
 
 
+class BurstCtl(object):
+
+    args = None
+
+    tcp_client = None
+
+    def __init__(self, args):
+        self.args = args
+
+    def make_send_box(self, cmd, username, password):
+        return Box(dict(
+            cmd=cmd,
+            body=json.dumps(
+                dict(
+                    auth=dict(
+                        username=username,
+                        password=password,
+                    )
+                )
+            )
+        ))
+
+    def output(self, s):
+        print '/' + '-' * 80
+        print s
+        print '-' * 80 + '/'
+
+    def process_stat(self):
+        send_box = self.make_send_box(constants.CMD_ADMIN_SERVER_STAT, self.args.username, self.args.password)
+        self.tcp_client.write(send_box)
+
+        rsp_box = self.tcp_client.read()
+
+        if not rsp_box:
+            self.output('disconnected.')
+            return False
+
+        if rsp_box.ret != 0:
+            self.output('fail. rsp_box.ret=%s' % rsp_box.ret)
+            return False
+
+        self.output(json.dumps(json.loads(rsp_box.body), indent=4))
+
+        return True
+
+    def run(self):
+        self.tcp_client = TcpClient(Box, self.args.host, self.args.port, self.args.timeout)
+
+        try:
+            self.tcp_client.connect()
+        except Exception, e:
+            self.output('connect fail: %s' % e)
+            return False
+
+        loop_times = 0
+
+        while True:
+
+            result = False
+            if self.args.cmd == 'stat':
+                result = self.process_stat()
+
+            if not result:
+                break
+
+            loop_times += 1
+            if loop_times >= self.args.loop > 0:
+                break
+
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                break
+
+        return True
+
+
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--host', help='burst admin host', action='store', default='127.0.0.1')
     parser.add_argument('-P', '--port', type=int, help='burst admin port', action='store', required=True)
     parser.add_argument('-u', '--username', help='username', action='store', default=None)
     parser.add_argument('-p', '--password', help='password', action='store', default=None)
-    parser.add_argument('-c', '--cmd', type=int, help='cmd', action='store', default=constants.CMD_ADMIN_SERVER_STAT)
+    parser.add_argument('-c', '--cmd', help='cmd', action='store', default='stat', choices=('stat',))
     parser.add_argument('-o', '--timeout', type=int, help='connect/send/receive timeout', action='store', default=10)
-    parser.add_argument('-l', '--loop', type=int, help='loop times, <=0 means infinite loop', action='store', default=1)
+    parser.add_argument('-l', '--loop', type=int, help='loop times, <=0 means infinite loop', action='store', default=-1)
     return parser
 
 
@@ -40,46 +117,11 @@ def send_and_recv(tcp_client, box):
         return True
 
 
-def run():
-    global debug
-
+def main():
     args = build_parser().parse_args()
-
-    tcp_client = TcpClient(Box, args.host, args.port, args.timeout)
-
-    try:
-        tcp_client.connect()
-    except Exception, e:
-        print 'connect fail: ', e
-        return
-
-    box = Box(dict(
-        cmd=args.cmd,
-        body=json.dumps(
-            dict(
-                auth=dict(
-                    username=args.username,
-                    password=args.password,
-                )
-            )
-        )
-    ))
-
-    loop_times = 0
-    while True:
-        if not send_and_recv(tcp_client, box):
-            break
-
-        loop_times += 1
-        if loop_times >= args.loop > 0:
-            break
-
-        try:
-            time.sleep(1)
-        except KeyboardInterrupt:
-            break
-
+    ctl = BurstCtl(args)
+    ctl.run()
 
 if __name__ == '__main__':
-    run()
+    main()
 
