1a8d8b010313f72d85785cc810b9535899e8c665
dantezhu <dantezhu@qq.com>
2016-02-24 20:12:16 +0800

    版本升级，不需要worker index，因为实在没必要. nginx就没有显示

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="burst",
-    version='1.0.26',
+    version='1.0.27',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
