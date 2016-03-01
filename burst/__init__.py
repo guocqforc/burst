be4bb4f159de2db8dc4d8374d7d1cbb6a508b1b6
dantezhu <dantezhu@qq.com>
2016-03-01 13:55:20 +0800

    版本号升级

--- burst/__init__.py
+++ burst/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.33'
+__version__ = '1.0.35'
 
 from .burst import Burst
 from .blueprint import Blueprint
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="burst",
-    version='1.0.33',
+    version='1.0.35',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
