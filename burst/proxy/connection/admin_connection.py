011bd26414e484646619538dc81715164674a239
dantezhu <dantezhu@qq.com>
2016-05-23 11:23:35 +0800

    items

--- burst/proxy/connection/admin_connection.py
+++ burst/proxy/connection/admin_connection.py
@@ -90,7 +90,7 @@ class AdminConnection(Protocol):
         else:
             if box.cmd == constants.CMD_ADMIN_SERVER_STAT:
                 workers = dict([(group_id, group_info['count']) for group_id, group_info in
-                                self.factory.proxy.app.config['GROUP_CONFIG']])
+                                self.factory.proxy.app.config['GROUP_CONFIG'].items()])
                 idle_workers = dict([(group_id, len(workers)) for group_id, workers in
                                      self.factory.proxy.task_dispatcher.idle_workers_dict.items()])
                 busy_workers = dict([(group_id, len(workers)) for group_id, workers in
