404a61d6a9090941878bb9801125ee251c8be65b
dantezhu <dantezhu@qq.com>
2016-05-26 20:34:28 +0800

    改一下文字和注释

--- burst/__init__.py
+++ burst/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.87'
+__version__ = '1.0.88'
 
 from .burst import Burst
 from .blueprint import Blueprint
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="burst",
-    version='1.0.87',
+    version='1.0.88',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
