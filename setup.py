47577c4bee0ce7492b4c6e5ab60bd647537285e2
dantezhu <dantezhu@qq.com>
2016-06-13 17:04:23 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="burst",
-    version='1.0.96',
+    version='1.0.97',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
