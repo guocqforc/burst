9f31c114f0e0edb5c738ecb8843d58f374d584ed
dantezhu <dantezhu@qq.com>
2016-06-15 18:21:52 +0800

    好吧，强迫症就是想升级版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="burst",
-    version='1.0.98',
+    version='1.0.99',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
